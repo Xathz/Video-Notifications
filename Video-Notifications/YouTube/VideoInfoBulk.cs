cb02473d9b734d39f2db10797301f8401a7d9c6e
Xathz <xathz@users.noreply.github.com>
2018-11-25 20:02:42 -0500

    Added ghetto pagination on getting video information
    Allows retrieving more than 50 videos in one call.

--- Video-Notifications/YouTube/VideoInfoBulk.cs
+++ Video-Notifications/YouTube/VideoInfoBulk.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Linq;
 using System.Collections.Generic;
 using Google.Apis.YouTube.v3;
 using Google.Apis.YouTube.v3.Data;
@@ -28,30 +29,54 @@ namespace VideoNotifications.YouTube {
                 // https://developers.google.com/youtube/v3/docs/videos/list
                 if (videoIDs.Count == 0) { return; }
 
-                VideosResource.ListRequest videoSearch = APIService.Videos.List("id,snippet,contentDetails");
-                videoSearch.Id = string.Join(",", videoIDs);
-                videoSearch.MaxResults = videoIDs.Count;
-                videoSearch.PrettyPrint = false;
+                List<List<string>> videoIDChunks = new List<List<string>>();
 
-                FullResponse = videoSearch.Execute();
+                SplitIntoChunksLoop:
+                if (videoIDs.Count > 0) {
 
-                IList<Video> videos = FullResponse.Items;
-                foreach (Video video in videos) {
-                    YouTubeVideo videoInfo = new YouTubeVideo {
-                        VideoID = video.Id,
-                        ChannelID = video.Snippet.ChannelId,
-                        Title = video.Snippet.Title,
-                        Description = video.Snippet.Description,
-                        Duration = TimeSpanUtils.ConvertDuration(video.ContentDetails.Duration),
-                        Posted = video.Snippet.PublishedAt,
-                        URL = $"https://www.youtube.com/watch?v={video.Id}",
-                        ThumbnailURL = GetBestThumbnail(video.Snippet.Thumbnails),
-                        Status = Status.Unwatched
-                    };
+                    if ((videoIDs.Count > 50) || (videoIDs.Count == 50)) {
+                        videoIDChunks.Add(videoIDs.Take(50).ToList());
+                        videoIDs.RemoveRange(0, 50);
+                    } else {
+                        videoIDChunks.Add(videoIDs.Take(videoIDs.Count).ToList());
+                        videoIDs.RemoveRange(0, videoIDs.Count);
+                    }
 
-                    Videos.Add(videoInfo);
-                    LoggingManager.Log.Info($"Video information processed for '{videoInfo.Title}' ({videoInfo.VideoID}) by channel ({videoInfo.ChannelID}).");
+                    goto SplitIntoChunksLoop;
                 }
+
+                LoggingManager.Log.Info($"Video ID chunk processing finished. {videoIDChunks.Count} total chunks.");
+                for (int i = 0; i < videoIDChunks.Count; i++) {
+                    LoggingManager.Log.Info($"Chunk #{i} contains {videoIDChunks[i].Count} videoIDs.");
+                }
+
+                foreach (List<string> chunk in videoIDChunks) {
+                    VideosResource.ListRequest videoSearch = APIService.Videos.List("id,snippet,contentDetails");
+                    videoSearch.Id = string.Join(",", chunk);
+                    videoSearch.MaxResults = chunk.Count;
+                    videoSearch.PrettyPrint = false;
+
+                    FullResponse = videoSearch.Execute();
+
+                    IList<Video> videos = FullResponse.Items;
+                    foreach (Video video in videos) {
+                        YouTubeVideo videoInfo = new YouTubeVideo {
+                            VideoID = video.Id,
+                            ChannelID = video.Snippet.ChannelId,
+                            Title = video.Snippet.Title,
+                            Description = video.Snippet.Description,
+                            Duration = TimeSpanUtils.ConvertDuration(video.ContentDetails.Duration),
+                            Posted = video.Snippet.PublishedAt,
+                            URL = $"https://www.youtube.com/watch?v={video.Id}",
+                            ThumbnailURL = GetBestThumbnail(video.Snippet.Thumbnails),
+                            Status = Status.Unwatched
+                        };
+
+                        Videos.Add(videoInfo);
+                        LoggingManager.Log.Info($"Video information processed for '{videoInfo.Title}' ({videoInfo.VideoID}) by channel ({videoInfo.ChannelID}).");
+                    }
+                }
+
             } catch (Exception ex) {
                 LoggingManager.Log.Error(ex, $"Failed to get bulk video information for: {string.Join(",", videoIDs)}.");
             }
